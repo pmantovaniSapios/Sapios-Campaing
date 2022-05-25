d509029773944bc8958248d19de98fd3f019ddab
Pedro Mantovani <pmantovani@sapios.com.br>
2022-05-25 11:38:05 -0300

    ajuste page

--- src/app.ts
+++ src/app.ts
@@ -35,13 +35,17 @@ app.get("/", (req: any, res: any) => {
     res.render("index");
 })
 
+app.get("/upload", (req: any, res: any) => {
+    res.render("upload");
+})
+
 app.post("/upload", upload.single("file"), (req: any, res: any) => {
     if (process.platform === "linux") {
         csvToJsonAndUpload(req.file.filename);
     } else {
         csvToJsonAndUpload(req.file.filename);
     }
-    res.render("/hap/upload");
+    res.redirect("/hap/upload");
 })
 
 const port = config.get<number>("port") || 7000;
